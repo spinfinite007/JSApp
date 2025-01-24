c664c71363aa00de17da6164fc1dc3063236d74d
Sergio Perez <sergioperez@nuviasmiles.com>
2025-01-24 12:30:07 -0700

    adding vue

--- gs-01-starting-project/app.js
+++ gs-01-starting-project/app.js
@@ -1,16 +1,32 @@
-const buttonEL = document.querySelector('button')
-const inputEL = document.querySelector('input')
-const listEL = document.querySelector('ul')
+Vue.createApp({
+    data() {
+        return {
+            goals: [],
+            enteredValue: ''
+        }
+    },
+    methods: {
+        addGoal() {
+            this.goals.push(this.enteredValue)
+        }
+    }
+}).mount('#app')
 
-// Need a function 
 
-function addGoal() {
-    const enteredValue = inputEL.value
-    const listItemEl = document.createElement('li')
-    listItemEl.textContent = enteredValue
-    listEL.appendChild(listItemEl)
+// const buttonEL = document.querySelector('button')
+// const inputEL = document.querySelector('input')
+// const listEL = document.querySelector('ul')
+
+// // Need a function 
+
+// function addGoal() {
+//     const enteredValue = inputEL.value
+//     const listItemEl = document.createElement('li')
+//     listItemEl.textContent = enteredValue
+//     listEL.appendChild(listItemEl)
+//     inputEL.value = ''
 
 
-}
+// }
 
-buttonEL.addEventListener('click', addGoal)
+// buttonEL.addEventListener('click', addGoal)
